4e992078b51b54e6a485f109debc255c2dfd487b
Rogan Creswick <creswick@gmail.com>
2014-03-23 11:55:02 -0700

    simpler types

--- src/NLP/Extraction/Extractor.hs
+++ src/NLP/Extraction/Extractor.hs
@@ -3,18 +3,14 @@ module NLP.Extraction.Extractor where
 
 import Data.Text (Text)
 import Text.Parsec.String () -- required for the `Stream [t] Identity t` instance.
-import Text.Parsec.Prim
-import Text.Parsec.Pos
+import Text.Parsec.Prim (parse, token, Parsec)
+import Text.Parsec.Pos  (newPos)
 
-import Data.Functor.Identity (Identity)
+import NLP.Types (TaggedSentence)
 
-import NLP.Types
+type Extractor = Parsec TaggedSentence ()
 
-type Extractor = forall u. Parsec TaggedSentence u
-
-posTok :: forall u.
-          (Stream TaggedSentence Identity (Text, Tag)) =>
-          Tag -> Parsec TaggedSentence u Text
+posTok :: Tag -> Parsec TaggedSentence () Text
 posTok tag = token showTok posFromTok testTok
   where
     showTok (_,t)    = show t
@@ -24,21 +20,8 @@ posTok tag = token showTok posFromTok testTok
 data Noun = N Text deriving (Read, Show, Eq, Ord)
 
 -- | Parse a noun ('nn') that immediately follows an article, returning the Noun
-parseNoun ::  forall u. Parsec TaggedSentence u Noun
+parseNoun :: Extractor Noun
 parseNoun = do
-  posTok $ Tag "at"
+  _ <- posTok $ Tag "at"
   n <- posTok $ Tag "nn"
   return $ N n
-
--- Extractor.hs:38:3:
---     No instance for (Stream [(Text, Tag)] Identity (Text, Tag))
---       arising from a use of `posTok'
---     Possible fix:
---       add an instance declaration for
---       (Stream [(Text, Tag)] Identity (Text, Tag))
---     In the expression: posTok
---     In a stmt of a 'do' block: posTok $ Tag "at"
---     In the expression:
---       do { posTok $ Tag "at";
---            n <- posTok $ Tag "nn";
---            return $ N n }
