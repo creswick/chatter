27b21fcd24d7c6b3cc80fe54fac8d5001f418017
Rogan Creswick <creswick@gmail.com>
2014-03-23 12:37:46 -0700

    simple examples of Parsec for information extraction

--- src/NLP/Extraction/Extractor.hs
+++ src/NLP/Extraction/Extractor.hs
@@ -1,27 +1,75 @@
 {-# LANGUAGE OverloadedStrings, RankNTypes, FlexibleContexts #-}
-module NLP.Extraction.Extractor where
+module NLP.Extraction.Extractor
+  ( parse
+  , txtTok
+  , posTok
+  , Noun(..)
+  , simpleNP
+  , nps
+  )
+where
 
 import Data.Text (Text)
+import Data.Maybe (catMaybes)
 import Text.Parsec.String () -- required for the `Stream [t] Identity t` instance.
-import Text.Parsec.Prim (parse, token, Parsec)
+import Text.Parsec.Prim (parse, token, Parsec, (<|>), many, try)
+-- import Text.Parsec.Combinator ( option )
 import Text.Parsec.Pos  (newPos)
 
-import NLP.Types (TaggedSentence)
+import NLP.Types (TaggedSentence, Tag(..))
 
 type Extractor = Parsec TaggedSentence ()
 
-posTok :: Tag -> Parsec TaggedSentence () Text
+-- | Consume a token with the given POS Tag
+posTok :: Tag -> Extractor (Text, Tag)
 posTok tag = token showTok posFromTok testTok
   where
-    showTok (_,t)    = show t
-    posFromTok (_,_) = newPos "unknown" 0 0
-    testTok (txt,t)  = if tag == t then Just txt else Nothing
+    showTok (_,t)       = show t
+    posFromTok (_,_)    = newPos "unknown" 0 0
+    testTok tok@(_,t) = if tag == t then Just tok else Nothing
 
-data Noun = N Text deriving (Read, Show, Eq, Ord)
+-- | Consume a token with the given lexical representation.
+txtTok :: Text -> Extractor (Text, Tag)
+txtTok txt = token showTok posFromTok testTok
+  where
+    showTok (t,_)     = show t
+    posFromTok (_,_)  = newPos "unknown" 0 0
+    testTok tok@(t,_) = if txt == t then Just tok else Nothing
 
--- | Parse a noun ('nn') that immediately follows an article, returning the Noun
-parseNoun :: Extractor Noun
-parseNoun = do
+-- | Consume any one non-empty token.
+anyToken :: Extractor (Text, Tag)
+anyToken = token showTok posFromTok testTok
+  where
+    showTok (txt,_)     = show txt
+    posFromTok (_,_)    = newPos "unknown" 0 0
+    testTok tok@(txt,_) | txt == "" = Nothing
+                        | otherwise  = Just tok
+
+data Noun = N Text
+  deriving (Read, Show, Eq, Ord)
+
+-- | Parse a noun ('nn') that immediately follows an article,
+-- returning the Noun
+simpleNP :: Extractor Noun
+simpleNP = do
   _ <- posTok $ Tag "at"
-  n <- posTok $ Tag "nn"
+  (n, _) <- someNN
   return $ N n
+
+someNN :: Extractor (Text, Tag)
+someNN = (try (posTok $ Tag "nn")) <|> (posTok $ Tag "nns")
+
+nps :: Extractor [Noun]
+nps = do
+  mNouns <- many (mnp <|> anyTok)
+  return $ catMaybes mNouns
+
+mnp :: Extractor (Maybe Noun)
+mnp =  try (do { n <- simpleNP
+               ; return $ Just n
+               })
+
+anyTok :: Extractor (Maybe Noun)
+anyTok = do
+  _ <- anyToken
+  return Nothing
