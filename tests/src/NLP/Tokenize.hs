b42ac1673b934846dc532987866de77784685fc9
Rogan Creswick <creswick@galois.com>
2015-07-24 10:37:04 -0700

    simplified the testing of the tokenizer

---
+++ tests/src/NLP/Tokenize.hs
@@ -0,0 +1,48 @@
+{-# LANGUAGE OverloadedStrings #-}
+module NLP.Tokenize where
+
+import Test.Tasty.HUnit
+import Test.Tasty (TestTree, testGroup)
+import Test.Tasty.QuickCheck (testProperty)
+
+import qualified Data.Map as Map
+import Data.Text (Text)
+import qualified Data.Text as T
+
+import NLP.Tokenize.TextNew
+
+import NLP.Types.Annotations
+import NLP.POS
+import qualified NLP.POS.LiteralTagger as LT
+
+import TestUtils
+
+tests :: TestTree
+tests = testGroup "NLP.Tokenze" $ map mkTokTest
+        [ ( "This is a test"
+          , [ (0, "This")
+            , (5, "is")
+            , (8, "a")
+            , (10, "test")
+            ])
+        , ("A url: http://google.com"
+          , [ (0, "A")
+            , (2, "url")
+            , (5, ":")
+            , (7, "http://google.com")
+            ])
+        ]
+
+mkTokSentence :: Text -> [(Int, Text)] -> TokenizedSentence
+mkTokSentence dat toks = TokSentence { tokText = dat
+                                     , tokAnnotations = map (mkTxtAnnotation dat) toks
+                                     }
+
+mkTxtAnnotation :: Text -> (Int, Text) -> Annotation Text Token
+mkTxtAnnotation dat (idx, tok) = Annotation (Index idx) (T.length tok) (Token tok) dat
+
+mkTokTest :: (Text, [(Int, Text)]) -> TestTree
+mkTokTest (dat, toks) = let actual = tokenize dat
+                            expected = mkTokSentence dat toks
+                        in testCase (T.unpack dat) (actual @?= expected)
+
