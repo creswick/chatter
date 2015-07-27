bae98b585e9762ec89beee92ddef8bc56dd154fb
Rogan Creswick <creswick@galois.com>
2015-07-27 13:39:27 -0700

    last shot at the text trie

--- src/NLP/Tokenize/TextTrie.hs
+++ src/NLP/Tokenize/TextTrie.hs
@@ -2,6 +2,7 @@
 module NLP.Tokenize.TextTrie
   where
 
+import qualified Data.Char as Char
 import qualified Data.Map as M
 import Data.Text (Text)
 import qualified Data.Text as T
@@ -19,21 +20,6 @@ testTrie = let t1 = PS.singleton "Test"
 buildTrie :: [Text] -> PS.TrieSet M.Map Char
 buildTrie toks = PS.fromList $ map T.unpack toks
 
-
-tokens :: [Text]
-tokens = [ "Galois, Inc."
-         , "Microsoft, Inc."
-         , "Apache Tomcat"
-         , "n't"
-         , "Mr."
-         , "Dr."
-         , "\""
-         , "testing"
-         , "test"
-         ]
-
-theTrie = buildTrie tokens
-
 trieChildren1 :: PS.TrieSet M.Map Char -> M.Map Char (PS.TrieSet M.Map Char)
 trieChildren1 trie = let m = PS.children1 trie
                      in if M.size m > 0
@@ -48,14 +34,18 @@ trieChildren1 trie = let m = PS.children1 trie
 
 -- | Build a tokenizer that tokenizes the supplied tokens, preffering longer matches where possible.
 protectTerms :: [Text] -> CaseSensitive -> RawToken -> [RawToken]
-protectTerms terms cs = protectedTokens $ buildTrie terms
+protectTerms terms cs =
+  let realTerms = case cs of
+                    Sensitive   -> terms
+                    Insensitive -> map T.toLower terms
+  in protectedTokens cs $ buildTrie realTerms
 
 -- | The incomming trie contains all strings that need to be tokenized
 -- specially.
 --
 -- TODO: Fails on the trie above [(test, testing)] and an input like "this is a testin thing"
-protectedTokens :: PS.TrieSet M.Map Char -> RawToken -> [RawToken]
-protectedTokens trie rawTok = reverse $ addLastToken $ T.foldl' fn emptyAcc $ text rawTok
+protectedTokens :: CaseSensitive -> PS.TrieSet M.Map Char -> RawToken -> [RawToken]
+protectedTokens cs trie rawTok = reverse $ addLastToken $ T.foldl' fn emptyAcc $ text rawTok
 
   where
     emptyAcc = Acc { curIdx = start rawTok
@@ -65,6 +55,7 @@ protectedTokens trie rawTok = reverse $ addLastToken $ T.foldl' fn emptyAcc $ te
                    , curText = []
                    , accTokens = []
                    , curTrie = trie
+                   , longestMatch = []
                    }
 
     addLastToken :: Accumulator -> [RawToken]
@@ -86,7 +77,10 @@ protectedTokens trie rawTok = reverse $ addLastToken $ T.foldl' fn emptyAcc $ te
     --    * Cur char does *not* match one of the next trie chars
     --  * No more tokens.
     fn :: Accumulator -> Char -> Accumulator
-    fn acc char = case M.lookup char $ trieChildren1 (curTrie acc) of
+    fn acc char = let ch = case cs of
+                             Sensitive   -> char
+                             Insensitive -> Char.toLower char
+                  in case M.lookup ch $ trieChildren1 (curTrie acc) of
       Nothing | inToken acc ->
                  case PS.member (reverse $ curText acc) trie of
                    True -> acc { curIdx = curIdx acc + 1
@@ -102,13 +96,14 @@ protectedTokens trie rawTok = reverse $ addLastToken $ T.foldl' fn emptyAcc $ te
                                                     : accTokens acc
                                , curTrie = trie -- reset the trie
                                }
-                   -- Nope! we are in an open token afterall (just finished a *prefix* of something in the trie)
+                   -- Nope! we are in an open token afterall
+                   -- (just finished a *prefix* of something in the trie)
                    False -> acc { curIdx = curIdx acc + 1
                                 , inToken = False
                                 , tokBuilder = OpenToken
                                 , curText = char:curText acc
                                 , curTrie = trie -- reset the trie
-                                }
+                                } -- TODO update this case.
               | otherwise   -> -- Still not in a special token:
                  acc { curIdx = curIdx acc + 1
                      , curText = char:curText acc -- append char to the current token
@@ -140,7 +135,7 @@ data Accumulator = Acc { curIdx :: !Int
                        , curText :: [Char]
                        , accTokens :: [RawToken]
                        , curTrie :: PS.TrieSet M.Map Char
---                       , longestToken :: [Char]
+                       , longestMatch :: Maybe [Char]
                        }
 
 
